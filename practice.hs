59616e9be66f410719ea0c4c5b9b2134a9041fd5
qngapparat <jakob.wallnoefer@outlook.com>
2017-08-28 17:37:04 +0200

    init

---
+++ practice.hs
@@ -0,0 +1,42 @@
+bmiTell :: (RealFloat a) => a -> a -> String
+bmiTell weight height
+	| weight / (height ^ 2) <= 18.5 = "lol skeletal"
+	| weight / (height ^ 2) <= 25 = "ok normal"
+	| weight / (height ^ 2) <= 30 = "fatty fuck off"
+	| otherwise = "naja die inneren werte zählen"
+ 
+bmiTell' :: (RealFloat a) => a->a->String
+bmiTell' weight height 
+	| bmi <= skinny = "lol skeletal xd"
+	| bmi <= normal = "ok normal" 
+	| bmi <= fat = "lol fattie" 
+	| otherwise = "fxcking obese dude"
+	where 
+		bmi = weight / height^2
+		(skinny, normal, fat) = (18.0,25.0,30.0)
+
+max' :: (Ord a) => a->a->a
+max' a b
+	| a < b = b
+	| otherwise = a
+
+compare' :: (Ord a) => a->a->Ordering
+a `compare'` b
+	| a < b = LT
+	| a > b = GT
+	| otherwise = EQ
+
+initials :: String -> String -> String
+initials firstName lastName = [f] ++ "." ++ [l] ++ "."
+	where
+		(f:_) = firstName
+		(l:_) = lastName
+
+calcBmis :: (RealFloat a) => [(a,a)] -> [a]
+calcBmis xs = [bmi w h | (w, h) <- xs]
+	where
+		bmi weight height = weight / height^2
+
+calcBmis' :: (RealFloat a) => [(a,a)] -> [a]
+calcBmis' xs = [bmi | (w,h) <- xs, let bmi = w / h^2, bmi < 25]
+
