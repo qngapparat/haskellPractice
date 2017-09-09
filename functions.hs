631ac1a2bb9dc51e453b9f2a2c18ed3a970109ec
qngapparat <jakob.wallnoefer@outlook.com>
2017-09-09 22:33:57 +0200

    added zipWith, map and filter

--- functions.hs
+++ functions.hs
@@ -3,4 +3,34 @@ divideByTen = (/10)
 
 divideByTen 20
 
-:!
+
+
+isUpper :: Char -> Bool
+isUpper = (`elem` ['A'..'Z'])
+
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x  = f (f x )
+
+
+zipWith' :: (a->b->c) -> a -> b -> c
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f x:xs y:ys = f x y : zipWith' f xs ys
+
+
+flip' :: (a->b->c) -> (b->a->c)
+flip' f x y  = f y x
+
+
+map' :: (a->b) -> [a] -> [b]
+map' _ [] = []
+map' f (x:xs) = f x : map' f xs
+
+
+filter' :: (a->Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' f (x:xs) 
+	| f x = x : filter' f xs --in case f x is True
+	| otherwise = filter' f xs
+
+ 
